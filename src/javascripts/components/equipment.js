c6a50296ef82bfbab6b5ae48fbffefd1e10185fd
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-03-16 17:49:24 -0500

    fixed delete button

--- src/javascripts/components/equipment.js
+++ src/javascripts/components/equipment.js
@@ -1,13 +1,13 @@
 const showEquipment = (arr) => {
   document.querySelector('#card-container').innerHTML = '';
-  document.querySelector('#title-container').innerHTML = 'Equipment';
+  document.querySelector('#title-container').innerHTML = '<h1>Equipment</h1>';
   arr.forEach((item) => {
     document.querySelector('#card-container').innerHTML += `<div class="card" style="width: 18rem;">
     <img src="${item.image}" class="card-img-top" alt="${item.title}">
     <div class="card-body">
       <h5 class="card-title">${item.title}</h5>
       <p class="card-text">${item.description}.</p>
-      <a href="#" class="btn btn-primary" id="deleteEquipment--${item.firebaseKey}>Delete</a>
+      <a href="#" class="btn btn-primary" id="deleteEquipment--${item.firebaseKey}">Delete</a>
     </div>
   </div>`;
   });
