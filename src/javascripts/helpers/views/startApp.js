eb55d5cc2ecdf1e476c7d355f8091ac47f453b27
tsekeres <twsekeres@gmail.com>
2021-03-15 21:09:42 -0500

    add domevents skeleton

--- src/javascripts/helpers/views/startApp.js
+++ src/javascripts/helpers/views/startApp.js
@@ -1,11 +1,13 @@
 import domBuilder from '../../components/domBuilder';
 import navBar from '../../components/navbar';
 import navigationEvents from '../events/navEvents';
+import domEvents from '../events/domEvents';
 
 const startApp = () => {
   domBuilder();
   navBar();
   navigationEvents();
+  domEvents();
 };
 
 export default startApp;
