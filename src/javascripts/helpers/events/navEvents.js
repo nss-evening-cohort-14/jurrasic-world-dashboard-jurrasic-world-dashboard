98d20d9f3519d70bf333f136b2790a1eae9d56a3
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-03-16 16:01:23 -0500

    Cards are now showing up on the DOM

--- src/javascripts/helpers/events/navEvents.js
+++ src/javascripts/helpers/events/navEvents.js
@@ -1,10 +1,21 @@
+import { showStaff, emptyStaff } from '../../components/cards/staff';
+import getStaff from '../data/staffData';
+
 const navigationEvents = () => {
   document.querySelector('#dinosaurs').addEventListener('click', () => {
     console.warn('CONNECTED');
   });
 
   document.querySelector('#staff').addEventListener('click', () => {
-    console.warn('CONNECTED');
+    document.querySelector('#title-container').innerHTML = '';
+    document.querySelector('#card-container').innerHTML = '';
+    getStaff().then((staffArray) => {
+      if (staffArray.length) {
+        showStaff(staffArray);
+      } else {
+        emptyStaff();
+      }
+    });
   });
 
   document.querySelector('#vendors').addEventListener('click', () => {
