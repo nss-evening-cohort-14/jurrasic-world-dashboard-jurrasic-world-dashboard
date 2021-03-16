09c2f58a9f60d696af44bc4033cbc3eadc8078c8
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-03-16 14:06:35 -0500

    added firebaseKey to sample data, tweaked card

--- src/javascripts/components/rides.js
+++ src/javascripts/components/rides.js
@@ -1,12 +1,13 @@
 const showRides = (arr) => {
   document.querySelector('#card-container').innerHTML = '';
+  document.querySelector('#title-container').innerHTML = 'Rides';
   arr.forEach((item) => {
     document.querySelector('#card-container').innerHTML += `<div class="card" style="width: 18rem;">
     <img src="${item.image}" class="card-img-top" alt="${item.name}">
     <div class="card-body">
       <h5 class="card-title">${item.name}</h5>
       <p class="card-text">${item.description}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href="#" class="btn btn-primary" id="deleteRide--${item.firebaseKey}">Delete</a>
     </div>
   </div>`;
   });
