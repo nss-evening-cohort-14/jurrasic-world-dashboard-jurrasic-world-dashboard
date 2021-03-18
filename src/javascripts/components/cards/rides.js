716c99b5a1d18914e7d3efcb33068218ad412ad9
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-03-18 14:19:18 -0500

    if user is not logged in, buttons disappear

--- src/javascripts/components/cards/rides.js
+++ src/javascripts/components/cards/rides.js
@@ -1,21 +1,36 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 const showRides = (arr) => {
   document.querySelector('#card-container').innerHTML = '';
   document.querySelector('#title-container').innerHTML = '<h1>Rides</h1>';
   document.querySelector('#add-button').innerHTML = '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal" id="add-ride-btn">Add a Ride</button>';
   document.querySelector('#form-container').innerHTML = '';
-  arr.forEach((item) => {
-    document.querySelector('#card-container').innerHTML += `<div class="card" style="width: 18rem;">
-    <img src="${item.image}" class="card-img-top" alt="${item.name}">
-    <div class="card-body">
-      <h5 class="card-title">${item.name}</h5>
-      <p class="card-text">${item.description}</p>
-      <div class="mt-auto">
-        <a href="#" class="btn btn-primary mx-2" id="edit-ride--${item.firebaseKey}" data-toggle="modal" data-target="#formModal">Edit</a>
-        <a href="#" class="btn btn-danger mx-2" id="delete-ride--${item.firebaseKey}">Delete</a>
+  if (firebase.auth().currentUser) {
+    arr.forEach((item) => {
+      document.querySelector('#card-container').innerHTML += `<div class="card" style="width: 18rem;">
+      <img src="${item.image}" class="card-img-top" alt="${item.name}">
+      <div class="card-body">
+        <h5 class="card-title">${item.name}</h5>
+        <p class="card-text">${item.description}</p>
+        <div class="mt-auto" id="ride-buttons">
+          <a href="#" class="btn btn-primary mx-2" id="edit-ride--${item.firebaseKey}" data-toggle="modal" data-target="#formModal">Edit</a>
+          <a href="#" class="btn btn-danger mx-2" id="delete-ride--${item.firebaseKey}">Delete</a>
+        </div>
       </div>
-    </div>
-  </div>`;
-  });
+    </div>`;
+    });
+  } else {
+    arr.forEach((item) => {
+      document.querySelector('#card-container').innerHTML += `<div class="card" style="width: 18rem;">
+      <img src="${item.image}" class="card-img-top" alt="${item.name}">
+      <div class="card-body">
+        <h5 class="card-title">${item.name}</h5>
+        <p class="card-text">${item.description}</p>
+      </div>
+    </div>`;
+    });
+  }
 };
 
 const emptyRides = () => {
