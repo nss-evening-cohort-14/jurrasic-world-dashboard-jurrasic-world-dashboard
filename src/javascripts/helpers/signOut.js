72ded96c57486271c31617c2eddddb3f7277529f
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-03-19 15:25:39 -0500

    deleted extra line in signOut

--- src/javascripts/helpers/signOut.js
+++ src/javascripts/helpers/signOut.js
@@ -2,10 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const signOut = () => {
-  document.querySelector('body').removeEventListener('click', domEvents);
-  document.querySelector('body').removeEventListener('click', navigationEvents);
   firebase.auth().signOut();
-  window.location.reload();
 };
 
 export default signOut;
