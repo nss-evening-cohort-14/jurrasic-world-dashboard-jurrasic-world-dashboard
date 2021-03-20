a7e1c38d0b72b7ba572efca7d642767d6c39f7f5
Greenfin17 <jmaple@yahoo.com>
2021-03-20 00:12:56 -0500

    corrected class syntax

--- src/javascripts/components/loadPage.js
+++ src/javascripts/components/loadPage.js
@@ -5,7 +5,7 @@ const loadPage = (array) => {
   document.querySelector('#title-container').innerHTML = '<h1 class="text-dark">Welcome to Jurassic World!</h1>';
 
   array.forEach((item) => {
-    document.querySelector('#card-container').innerHTML += `<div class="card border-danger" id="view-card-style" style="width: 10rem;">
+    document.querySelector('#card-container').innerHTML += `<div class="card border-danger view-card-style" style="width: 10rem;">
       <a href="#" id="card-top--${item.firebaseKey}"><img class="card-top-img pt-2" src=${item.image} id="${item.firebaseKey}" alt="View Card" style="width: 70px; height: auto"></a>
       <div class="card-body pointer" id="card-body--${item.firebaseKey}">
         <a href="#"><h5 id="${item.firebaseKey}" class="title">${item.title}</h5></a>
