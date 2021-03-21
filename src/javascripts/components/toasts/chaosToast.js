dd884b84e2ca2f4b8de0437f83c55c47cabff457
Greenfin17 <jmaple@yahoo.com>
2021-03-21 01:16:59 -0500

    chaos with flying monkey, random messages, start/stop by click

--- src/javascripts/components/toasts/chaosToast.js
+++ src/javascripts/components/toasts/chaosToast.js
@@ -6,9 +6,8 @@ const chaosToast = (message) => {
     <div class="toast"  id="chaosToast" role="alert" aria-live="assertive" 
       data-delay="5000" aria-atomic="true">
       <div class="toast-header">
-        <img src="${caution} class="rounded mr-2" alt="...">
-        <strong class="mr-auto">Bootstrap</strong>
-        <small>11 mins ago</small>
+      <img src="${caution}" class="rounded mr-2" style="width: 50px; height: auto;" alt="warning sign"> 
+        <strong class="mr-auto">Warning!</strong>
         <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
