d7720f297c4c48bdf301ed20139512a0a1a2819d
elliewalkersm <elliewalkersm@gmail.com>
2021-04-13 21:00:44 -0500

    added close x on alert

--- src/javascripts/components/buttons/equipmentButton.js
+++ src/javascripts/components/buttons/equipmentButton.js
@@ -7,13 +7,15 @@ const checkEquipment = () => {
       // show alert with names
       const names = broken.map((item) => item.title);
       const joinedNames = names.join(', ');
-      document.querySelector('#toast-container').innerHTML = `<div class="alert alert-danger" role="alert">
-      The following equipment is currently broken: ${joinedNames}.
-    </div>`;
+      document.querySelector('#toast-container').innerHTML = `<div class="alert alert-danger alert-dismissible fade show w-25 p-3" role="alert">
+        The following equipment is currently broken: ${joinedNames}.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>`;
     } else {
       // show no broken equipment alert
-      document.querySelector('#toast-container').innerHTML = `<div class="alert alert-danger" role="alert">
+      document.querySelector('#toast-container').innerHTML = `<div class="alert alert-success alert-dismissible fade show w-25 p-3" role="alert">
       There is currently no broken equipment.
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>`;
     }
   });
