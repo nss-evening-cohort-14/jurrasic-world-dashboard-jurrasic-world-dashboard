2a24014565b9b50bd3207916132d907fee40a0c4
elliewalkersm <elliewalkersm@gmail.com>
2021-04-13 20:22:58 -0500

    equipment broken alert functional

--- src/javascripts/components/buttons/equipmentButton.js
+++ src/javascripts/components/buttons/equipmentButton.js
@@ -6,9 +6,15 @@ const checkEquipment = () => {
     if (broken.length) {
       // show alert with names
       const names = broken.map((item) => item.title);
-      console.warn(names.join(', '));
+      const joinedNames = names.join(', ');
+      document.querySelector('#toast-container').innerHTML = `<div class="alert alert-danger" role="alert">
+      The following equipment is currently broken: ${joinedNames}.
+    </div>`;
     } else {
       // show no broken equipment alert
+      document.querySelector('#toast-container').innerHTML = `<div class="alert alert-danger" role="alert">
+      There is currently no broken equipment.
+    </div>`;
     }
   });
 };
