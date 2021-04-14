afcdab20b2790faad67cb6c32403b23c5fa20d9f
elliewalkersm <elliewalkersm@gmail.com>
2021-04-13 21:06:40 -0500

    ignore

--- src/javascripts/components/buttons/equipmentButton.js
+++ src/javascripts/components/buttons/equipmentButton.js
@@ -9,13 +9,17 @@ const checkEquipment = () => {
       const joinedNames = names.join(', ');
       document.querySelector('#toast-container').innerHTML = `<div class="alert alert-danger alert-dismissible fade show w-25 p-3" role="alert">
         The following equipment is currently broken: ${joinedNames}.
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
       </div>`;
     } else {
       // show no broken equipment alert
-      document.querySelector('#toast-container').innerHTML = `<div class="alert alert-success alert-dismissible fade show w-25 p-3" role="alert">
+      document.querySelector('#toast-container').innerHTML = `<div class="alert alert-success alert-dismissible fade show" role="alert">
       There is currently no broken equipment.
-      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
     </div>`;
     }
   });
